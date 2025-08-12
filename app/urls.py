5976281e78aa6f7aa2827758e7bcf4dfa588cff6
lotinmaria <mariaholotin2007>
2025-08-12 16:52:32 -0300

    FEAT: Criação da livraria

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
+from uploader.router import router as uploader_router
 
-from core.views import UserViewSet
+from core.views import CategoriaViewSet, EditoraViewSet, UserViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
-
-router.register(r'usuarios', UserViewSet, basename='usuarios')
+router.register(r"livros", LivroViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"categorias", CategoriaViewSet) 
+router.register(r"autores", AutorViewSet)
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +35,8 @@ urlpatterns = [
     ),
     # API
     path('api/', include(router.urls)),
+    path("api/media/", include(uploader_router.urls)),
+
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
